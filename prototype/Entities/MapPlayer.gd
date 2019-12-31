fe458f6ff517662b195aa80530091f50e52e399a
nightblade9 <nightbladecodes@gmail.com>
2019-12-31 13:06:31 -0500

    Player moves to WASD/arrows

---
+++ prototype/Entities/MapPlayer.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+const MOVE_SPEED:int = 200 # px/s
+var _velocity:Vector2 = Vector2.ZERO
+
+func _process(delta):
+	_velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_up"):
+		_velocity.y = -MOVE_SPEED
+	elif Input.is_action_pressed("ui_down"):
+		_velocity.y = MOVE_SPEED
+	
+	if Input.is_action_pressed("ui_left"):
+		_velocity.x = -MOVE_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		_velocity.x = MOVE_SPEED
+	
+	self.position += _velocity * delta
