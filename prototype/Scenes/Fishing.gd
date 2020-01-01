f03636f7e8ba0b54a0e63679593e1622afdfb339
nightblade9 <nightbladecodes@gmail.com>
2020-01-01 15:25:22 -0500

    You can reset/retrieve bobber in fishing

--- prototype/Scenes/Fishing.gd
+++ prototype/Scenes/Fishing.gd
@@ -4,12 +4,11 @@ const PROGRESS_BAR_VELOCITY = 96 # 960 = ~full cycle in 1s
 
 var _horizontal_percent = -1
 var _vertical_percent = -1
-
-var _casting_horizontally = true
 var _increasing_value = true
 
 onready var HORIZONTAL_RANGE = $Ocean.margin_right - $RiverBank.margin_right - $Bobber.get_child(0).margin_right
 onready var VERTICAL_RANGE = $Ocean.margin_bottom - $Ocean.margin_top - $Bobber.get_child(0).margin_bottom
+onready var BOBBER_HOME = $Bobber.position
 
 func _process(delta):
 	if _horizontal_percent == -1 or _vertical_percent == -1:
@@ -23,9 +22,8 @@ func _process(delta):
 				_increasing_value = true
 		
 		if Input.is_action_just_pressed("ui_accept"):
-			if _casting_horizontally:
+			if _horizontal_percent == -1:
 				_horizontal_percent = $ProgressBar.value
-				_casting_horizontally = false
 				$ProgressBar/Label.text = "Horizontal: " + str(_horizontal_percent) + "%"
 				$ProgressBar.value = 0
 				$Bobber.position.x = $RiverBank.margin_right + (_horizontal_percent / 100.0) * HORIZONTAL_RANGE
@@ -34,4 +32,8 @@ func _process(delta):
 				$ProgressBar/Label.text += "\nVertical: " + str(_vertical_percent) + "%"
 				$ProgressBar.value = 0
 				$Bobber.position.y = $Ocean.margin_top + (_vertical_percent / 100.0) * VERTICAL_RANGE
-				
+	elif Input.is_action_just_pressed("ui_accept"):
+		$Bobber.position = BOBBER_HOME
+		$ProgressBar/Label.text = ""
+		_horizontal_percent = -1
+		_vertical_percent = -1
