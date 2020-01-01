3ba78929e502967f3a9672b5e8efc5f48d58e1f1
nightblade9 <nightbladecodes@gmail.com>
2019-12-31 23:23:38 -0500

    NPCs speak to you in a handy dandy window!

--- prototype/Entities/NPC.gd
+++ prototype/Entities/NPC.gd
@@ -1,7 +1,18 @@
 extends Area2D
 
+const DialogPanel = preload("res://UI/DialogPanel.tscn")
+
 export var dialog:String = ""
+var _panel#:DialogPanel
 
 func _on_Door_body_entered(body):
 	if body.name == "MapPlayer":
-		print(dialog)
+		_panel = DialogPanel.instance()
+		_panel.set_text(dialog)
+		_panel.position.y += 100
+		add_child(_panel)
+
+func _on_NPC_body_exited(body):
+	if body.name == "MapPlayer":
+		remove_child(_panel)
+		_panel.free()
---
+++ prototype/UI/DialogPanel.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func set_text(text):
+	$Label.text = text
