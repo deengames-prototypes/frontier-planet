58b405b75e2701308a439351f22c4d3b3b9c90ec
nightblade9 <nightbladecodes@gmail.com>
2020-01-04 23:52:13 -0500

    Items can only be picked once per day Lots of hacks.

--- prototype/Entities/World/Items/Pickable.gd
+++ prototype/Entities/World/Items/Pickable.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
+signal on_picked_up
+
 func _on_body_entered(body):
 	if body.name == "MapPlayer":
 		Globals.player_inventory.append(self.name.to_lower())
-		self.get_parent().remove_child(self)
-		self.queue_free()
+		emit_signal("on_picked_up")
+		call_deferred("_remove_me")
+
+func _remove_me():
+	self.get_parent().remove_child(self)
+	self.queue_free()
--- prototype/Scenes/Forest.gd
+++ prototype/Scenes/Forest.gd
@@ -11,15 +11,21 @@ func _ready():
 		# SPAWN MORE OVERLORDS!
 		# TODO: could be more dynamic
 		if spawn_what == "Morel":
-			_spawn(Morel, spot_num)
+			_spawn(Morel, spot_num, "Morel")
 		elif spawn_what == "Mushroom":
-			_spawn(Mushroom, spot_num)
+			_spawn(Mushroom, spot_num, "Mushroom")
 		else: # Herb
-			_spawn(Herb, spot_num)
+			_spawn(Herb, spot_num, "Herb")
 		spot_num += 1
 				
-func _spawn(what, spot_num):
+func _spawn(what, spot_num, item):
 	var where = $ResourceSpot.get_node("Spot" + str(spot_num)).position
 	var instance = what.instance()
 	instance.position = where
 	add_child(instance)
+	# Hack: remove the first matching item
+	instance.connect("on_picked_up", self, "_remove_forest_item", [item])
+
+# Hack: remove the first matching item
+func _remove_forest_item(item):
+	Globals.forest_items.remove(Globals.forest_items.find(item))
