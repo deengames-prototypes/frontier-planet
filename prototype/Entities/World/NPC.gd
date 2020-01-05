0057a66e77261bb0a06be54be394f4513886a155
nightblade9 <nightbladecodes@gmail.com>
2020-01-04 22:49:04 -0500

    POC quest flavour texts for farmour boi

--- prototype/Entities/World/NPC.gd
+++ prototype/Entities/World/NPC.gd
@@ -2,10 +2,14 @@ extends Area2D
 
 const DialogPanel = preload("res://UI/DialogPanel.tscn")
 
-export var dialog:String = ""
+export var quest_dialogs:Array = []
+export var quest_requires:Array = []
+export var quest_completions:Array = []
 var _panel#:DialogPanel
+var current_quest_number = 0
 
 func _on_Door_body_entered(body):
+	var dialog = quest_dialogs[current_quest_number]
 	if body.name == "MapPlayer":
 		_panel = DialogPanel.instance()
 		_panel.set_text(dialog)
