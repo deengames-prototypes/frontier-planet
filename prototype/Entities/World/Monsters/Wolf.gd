3bdfb1430f02d87fa2bc6abff16733d4805a1c63
nightblade9 <nightbladecodes@gmail.com>
2020-01-08 22:27:58 -0500

    Fix: wolves hunt properly

--- prototype/Entities/World/Monsters/Wolf.gd
+++ prototype/Entities/World/Monsters/Wolf.gd
@@ -1,37 +1,36 @@
 extends "res://Entities/World/Monsters/HurtPlayerOnTouch.gd"
 
-const HUNTING_RANGE = 9500
-const VELOCITY = 200
+const HUNTING_RANGE = 500
+const VELOCITY = 175
+const RECHARGE_MULTIPLIER = 2 # 4 = recharge 5x as fast as exhaustion
 
 var MAX_ENERGY_SECONDS = 5
 var _energy_left_seconds = MAX_ENERGY_SECONDS
 var _resting_since = 0
 
+var print_stuff = false
+
 func _ready():
 	damage_per_second = 40
 
 func _process(delta):
-	var distance = sqrt(pow(position.x - Globals.player.position.x, 2)
-		+ pow(position.y - Globals.player.position.y, 2))
+	var player_pos = Globals.player.global_position
+	var my_pos = self.global_position
 	
-	#var d = (Globals.player.position - self.position).normalized()
-	#print(str(d * VELOCITY * delta))
-	#self.position += d * VELOCITY * delta
-	
-	#return
+	var distance = sqrt(pow(my_pos.x - player_pos.x, 2)
+		+ pow(my_pos.y - player_pos.y, 2))
 	
 	if _resting_since == 0 and distance <= HUNTING_RANGE:
 		if _energy_left_seconds > 0:
-			var direction = (Globals.player.position - self.position).normalized()
+			
+			var direction = my_pos.direction_to(player_pos)
 			self.position += direction * VELOCITY * delta
 			_energy_left_seconds -= delta
 		else:
 			_resting_since = OS.get_ticks_msec()
-			print("resting start")
 	else:
 		if _energy_left_seconds < MAX_ENERGY_SECONDS:
-			_energy_left_seconds += delta
+			_energy_left_seconds += delta * RECHARGE_MULTIPLIER
 			if _energy_left_seconds >= MAX_ENERGY_SECONDS:
 				_energy_left_seconds = MAX_ENERGY_SECONDS
 				_resting_since = 0
-				print("done resting")
