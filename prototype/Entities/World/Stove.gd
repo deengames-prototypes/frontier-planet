15ba3e6f2b88feec6ab4c59ee0ffb2554b191585
nightblade9 <nightbladecodes@gmail.com>
2020-01-09 20:43:10 -0500

    Cooking works.

--- prototype/Entities/World/Stove.gd
+++ prototype/Entities/World/Stove.gd
@@ -11,4 +11,18 @@ func _process(delta):
 		$Label.text = "Stove"
 	
 func _on_use_stove():
-	$Label.text = "Used the stove."
+	$Label.text = "Not enough ingredients for an omelet."
+	if len(Globals.player_inventory) >= 2 and Globals.player_inventory.find("egg") > -1:
+		var egg = Globals.player_inventory[Globals.player_inventory.find("egg")]
+		var ingredient = Globals.player_inventory[0]
+		for item in Globals.player_inventory:
+			if item != "egg":
+				ingredient = item
+				break
+		
+		Globals.player_inventory.erase(egg)
+		Globals.player_inventory.erase(ingredient)
+		
+		var omelet = ingredient + " omelet"
+		Globals.player_inventory.append(omelet)
+		$Label.text = "Made " + omelet
