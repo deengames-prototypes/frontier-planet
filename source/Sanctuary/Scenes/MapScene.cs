909257d7ef21b4c759e39bebf76b4b2347c4c095
nightblade9 <nightbladecodes@gmail.com>
2020-01-31 16:03:45 -0500

    Basic grass tile and player movement on map scene.

--- source/Sanctuary/Scenes/MapScene.cs
+++ source/Sanctuary/Scenes/MapScene.cs
@@ -1,13 +1,28 @@
+using System.IO;
 using Puffin.Core;
 using Puffin.Core.Ecs;
+using Puffin.Core.Tiles;
 
 namespace DeenGames.Sanctuary.Scenes
 {
     public class MapScene : Scene
     {
+        private const int MAP_WIDTH = 30;
+        private const int MAP_HEIGHT = 20;
         public MapScene()
         {
-            this.Add(new Entity().Label("HELLO!"));
+            var tilemap = new TileMap(MAP_WIDTH, MAP_HEIGHT, Path.Join("Content", "Images", "Tilesets", "Outside.png"), 32, 32);
+            tilemap.Define("Grass", 0, 0);
+            for (var y = 0; y < MAP_HEIGHT; y++)
+            {
+                for (var x = 0; x < MAP_WIDTH; x++)
+                {
+                    tilemap.Set(x, y, "Grass");
+                }
+            }
+
+            this.Add(tilemap);
+            this.Add(new Entity().Colour(0xFFFFFF, 32, 32).FourWayMovement(100).Move(100, 50));
         }
     }
 }
