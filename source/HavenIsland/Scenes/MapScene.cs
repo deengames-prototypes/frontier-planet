96699668c94b7d0b7fb3f8e7bea95874cc1a395f
nightblade9 <nightbladecodes@gmail.com>
2020-02-17 16:20:14 -0500

    Show energy bar

--- source/HavenIsland/Scenes/MapScene.cs
+++ source/HavenIsland/Scenes/MapScene.cs
@@ -10,6 +10,9 @@ namespace DeenGames.HavenIsland.Scenes
         private const int MAP_WIDTH = 40;
         private const int MAP_HEIGHT = 23;
 
+        // We don't have a concept like CanvasLayer, but we can reposition relative to camera
+        private Entity energyBar;
+
         public MapScene()
         {
             var groundTileMap = new TileMap(MAP_WIDTH, MAP_HEIGHT, Path.Join("Content", "Images", "Tilesets", "Outside.png"), 32, 32);
@@ -36,6 +39,13 @@ namespace DeenGames.HavenIsland.Scenes
                 .Spritesheet(Path.Combine("Content", "Images", "Characters", "Protagonist.png"), 26, 32)
                 .Move(400, 300).FourWayMovement(100)
                 .Collide(26, 32, true));
+
+            // TODO: probably backed by a PNG
+            // TODO: show/hide label on mouse over/out
+            this.energyBar = new Entity().Move(HavenIslandGame.LatestInstance.Width - 24, HavenIslandGame.LatestInstance.Height - 108)
+                .Colour(0xf4b41b, 8, 100);
+                
+            this.Add(this.energyBar);
         }
     }
 }
