0f3d437d878d8a6b2bf81ff234d3630ebfd275ac
nightblade9 <nightbladecodes@gmail.com>
2020-02-16 20:33:14 -0500

    Pixel-art protagonist Based on template-man recolour

--- source/HavenIsland/Scenes/MapScene.cs
+++ source/HavenIsland/Scenes/MapScene.cs
@@ -9,6 +9,7 @@ namespace DeenGames.HavenIsland.Scenes
     {
         private const int MAP_WIDTH = 40;
         private const int MAP_HEIGHT = 23;
+
         public MapScene()
         {
             var tilemap = new TileMap(MAP_WIDTH, MAP_HEIGHT, Path.Join("Content", "Images", "Tilesets", "Outside.png"), 32, 32);
@@ -22,6 +23,8 @@ namespace DeenGames.HavenIsland.Scenes
             }
 
             this.Add(tilemap);
+
+            this.Add(new Entity().Spritesheet(Path.Combine("Content", "Images", "Characters", "Protagonist.png"), 26, 32).Move(400, 300).FourWayMovement(200)); 
         }
     }
 }
