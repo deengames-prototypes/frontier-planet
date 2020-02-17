527162ac85e2d97ddec5eaf388161f0365e725ad
nightblade9 <nightbladecodes@gmail.com>
2020-02-16 21:06:51 -0500

    First cut of character + tree. It looks hideous, gotta change some stuff.

--- source/HavenIsland/Scenes/MapScene.cs
+++ source/HavenIsland/Scenes/MapScene.cs
@@ -12,19 +12,30 @@ namespace DeenGames.HavenIsland.Scenes
 
         public MapScene()
         {
-            var tilemap = new TileMap(MAP_WIDTH, MAP_HEIGHT, Path.Join("Content", "Images", "Tilesets", "Outside.png"), 32, 32);
-            tilemap.Define("Grass", 0, 0);
+            var groundTileMap = new TileMap(MAP_WIDTH, MAP_HEIGHT, Path.Join("Content", "Images", "Tilesets", "Outside.png"), 32, 32);
+            groundTileMap.Define("Grass", 0, 0);
             for (var y = 0; y < MAP_HEIGHT; y++)
             {
                 for (var x = 0; x < MAP_WIDTH; x++)
                 {
-                    tilemap.Set(x, y, "Grass");
+                    groundTileMap.Set(x, y, "Grass");
                 }
             }
 
-            this.Add(tilemap);
+            this.Add(groundTileMap);
 
-            this.Add(new Entity().Spritesheet(Path.Combine("Content", "Images", "Characters", "Protagonist.png"), 26, 32).Move(400, 300).FourWayMovement(200)); 
+            var objectsTileMap = new TileMap(MAP_WIDTH, MAP_HEIGHT, Path.Join("Content", "Images", "Tilesets", "Tree.png"), 27, 32);
+            objectsTileMap.Define("TreeTop", 0, 0, true);
+            objectsTileMap.Define("TreeTrunk", 0, 1, true);
+            objectsTileMap.Set(20, 10, "TreeTop");
+            objectsTileMap.Set(20, 11, "TreeTrunk");
+            
+            this.Add(objectsTileMap);
+
+            this.Add(new Entity()
+                .Spritesheet(Path.Combine("Content", "Images", "Characters", "Protagonist.png"), 26, 32)
+                .Move(400, 300).FourWayMovement(100)
+                .Collide(26, 32, true));
         }
     }
 }
